21f9503e301180bdfd2cce034ff0e0d875948f10
iftach shalev <iftachshalev30115@gmail.com>
2020-11-08 19:17:51 +0200

    setup

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-# from win32gui import GetWindowText, GetForegroundWindow
-# from time import sleep
-#
-# while True:
-#     sleep(0.5)
-#     print(GetWindowText(GetForegroundWindow()))
-#     if GetWindowText(GetForegroundWindow()) == "ThoughtsAndStreams – try.py PyCharm":
-#         print(1)
-#!/usr/bin/env python
-
-"""Find the currently active window."""
 
 import logging
 import sys
